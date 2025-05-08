9640106bcc2fdbb0f79d97a88a17f865a1f474af
JesseeDantonio <dyland.antonio.dev42@gmail.com>
2025-05-08 11:00:08 +0200

    feat: ajout de la police Poppins via Google Fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'Classic Studio',
+      link: [
+        {
+          href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap',
+          rel: 'stylesheet'
+        }
+      ]
     }
   },
   compatibilityDate: '2024-11-01',
