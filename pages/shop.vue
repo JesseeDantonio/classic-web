4d9446c2d018974f4f5983c97f2400f4d1d94364
JesseeDantonio <dyland.antonio.dev42@gmail.com>
2025-05-08 11:15:36 +0200

    feat: ajout du composant Reassurance dans la page boutique pour qu'il prenne effet

--- pages/shop.vue
+++ pages/shop.vue
@@ -5,6 +5,7 @@
         </template>
         <template v-slot:main>
             <ShopDiscover title="Découvre nos offres !" description="Contribue au développement de nos projets actuels et avenir."></ShopDiscover>
+            <Reassurance></Reassurance>
         </template>
         <template v-slot:footer>
             <Links></Links>
@@ -17,4 +18,5 @@ import Layout from '../components/Layout.vue';
 import Navbar from '../components/Navbar.vue';
 import Links from '../components/Links.vue';
 import ShopDiscover from '@/components/ShopDiscover.vue';
+import Reassurance from '@/components/Reassurance.vue'
 </script>
